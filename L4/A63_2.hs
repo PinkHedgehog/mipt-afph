79c2020893f5af4a99b27089b290ae2a867dfcd6
Andrey Ivanov <ivanov.as@phystech.edu>
2022-03-05 17:34:15 +0300

    liftA2 explaination

--- L4/A63_2.hs
+++ L4/A63_2.hs
@@ -1,12 +1,17 @@
 import Data.List
 import Control.Applicative (liftA2)
+{-
+in context of main and IO:
+liftA2 :: (a  -> b      -> c)     -> IO a           -> IO b      -> IO c
+          solve                      read <$> getLine  getLine
+          Int -> String -> String -> IO Int         -> IO String -> IO String
 
+*liftA2* makes "regular" function *solve* take "irregular" values
+*read <$> getLine*, whose type is *IO Int*, and *getLine* of type *IO String*,
+instead of *Int* and *String*, respectively
+-} 
 solve :: Int -> String -> String
 solve n input = unwords $ map (words input !!) [0,2..n-1]
 
 main :: IO ()
-main = do
-    -- n' <- getLine
-    -- let n = read n' :: Int
-    -- numbers <- getLine
-    putStrLn =<< liftA2 solve (read <$> getLine) getLine
+main = putStrLn =<< liftA2 solve (read <$> getLine) getLine
